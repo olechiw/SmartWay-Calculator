539f2f8d5ae5cbf1810bcb32edae07200bc1eae2
Olechiw <olechiw.jakob@epa.gov>
2018-06-26 17:39:02 -0400

    Started work on all of the constants for smartway bin rankings

--- smartwayPerformance.js
+++ smartwayPerformance.js
@@ -1,5 +1,39 @@
 // File for all of the performance rankings and their corresponding pollution coeffecients
-function AutoRankings()
-{
 
+class Type
+{
+    constructor (rankGroupTonMi, rankGroupMi)
+    {
+        // For units of TonMile
+        this.tonMi = rankGroupTonMi
+        // For units of just Mile
+        this.Mi = rankGroupMi
+    }
 }
+
+class RankGroup
+{
+    constructor (r1, r2, r3, r4, r5, ns)
+    {
+        this.rank1 = r1
+        this.rank2 = r2
+        this.rank3 = r3
+        this.rank4 = r4
+        this.rank5 = r5
+        this.NS = ns
+    }
+}
+
+class PollutionCoeffecient
+{
+    constructor (C, N, P)
+    {
+        // Pollution per Tonn Mile
+        this.CO2 = C
+        this.NOx = N
+        this.PM = P
+    }
+}
+
+
+//TODO: WRITE A JSON AND PARSE IT FOR THE DIFFERENT CONSTANTS
